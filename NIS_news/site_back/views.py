a7acece60d156885c12223813c6c2e4166b40c87
c0st1nus <Koshevoy_K8030@kst.nis.edu.kz>
2024-01-08 19:20:08 +0600

    added 2 objects in database start of creating database interaction system

--- NIS_news/site_back/views.py
+++ NIS_news/site_back/views.py
@@ -4,4 +4,5 @@ from .models import Article
 
 def index(request):
     articles = Article.objects.all()
+    print(articles)
     return render(request, 'site_back/index.html', {'articles': articles})
