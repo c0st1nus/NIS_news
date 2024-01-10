91af61fdb1dae51021c75c5aa5d467194375d141
c0st1nus <Koshevoy_K8030@kst.nis.edu.kz>
2024-01-10 22:42:46 +0600

    added tags search system

--- NIS_news/NIS_news/urls.py
+++ NIS_news/NIS_news/urls.py
@@ -17,12 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from site_back.views import index
+from site_back.views import index, tag_viev
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='administrator'),
     path('', index, name='main'),
     path('news/', index, name='news'),
     path('admission/', index, name='admission'),
-    path('media_club/', index, name='media_club')
+    path('media_club/', index, name='media_club'),
+    path('tags/<str:tag>/', tag_viev, name='tags'),
 ]
--- NIS_news/site_back/views.py
+++ NIS_news/site_back/views.py
@@ -6,3 +6,7 @@ def index(request):
     articles = Article.objects.all()
     print(articles)
     return render(request, 'site_back/index.html', {'articles': articles})
+
+def tag_viev(request, tag):
+    articles = Article.objects.filter(tags=tag)
+    return render(request, 'site_back/index.html', {'articles': articles})
